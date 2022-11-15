12992f37f3c0c29215c2ab9461dfe5f2ad8fc31c
zhihao.ouyang <zhihao.ouyang@bytedance.com>
2022-11-14 17:19:59 -0800

    gradio demo for stable diffusion model

---
+++ gradio/install.sh
@@ -0,0 +1,4 @@
+#huggingface-cli login
+#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
+pip install --upgrade diffusers transformers scipy transformer ftfy accelerate
+python try.py
---
+++ gradio/run.sh
@@ -0,0 +1,6 @@
+#huggingface-cli login
+#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
+
+auth_token=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
+#CUDA_VISIBLE_DEVICES=0 python diffusion_gradio.py
+python diffusion_gradio.py
