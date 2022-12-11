a5b1acee4e51188258b6b77c033b2aa6a1dabe4b
zhihao.ouyang <zhihao.ouyang@bytedance.com>
2022-12-11 23:21:23 +0800

    demo v0.1

--- figma_plugin/code.js
+++ figma_plugin/code.js
@@ -2,18 +2,22 @@
 // This file holds the main code for the plugin. It has access to the *document*.
 // You can access browser APIs such as the network by creating a UI which contains
 // a full browser environment (see documentation).
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 // Runs this code if the plugin is run in Figma
 if (figma.editorType === 'figma') {
     // This plugin will open a window to prompt the user to enter a number, and
     // it will then create that many rectangles on the screen.
     // This shows the HTML page in "ui.html".
     figma.showUI(__html__);
-    // Calls to "parent.postMessage" from within the HTML page will trigger this
-    // callback. The callback will be passed the "pluginMessage" property of the
-    // posted message.
-    figma.ui.onmessage = msg => {
-        // One way of distinguishing between different types of messages sent from
-        // your HTML page is to use an object with a "type" property like this.
+    figma.ui.onmessage = (msg) => __awaiter(void 0, void 0, void 0, function* () {
         if (msg.type === 'create-shapes') {
             const nodes = [];
             for (let i = 0; i < msg.count; i++) {
@@ -26,10 +30,26 @@ if (figma.editorType === 'figma') {
             figma.currentPage.selection = nodes;
             figma.viewport.scrollAndZoomIntoView(nodes);
         }
-        // Make sure to close the plugin when you're done. Otherwise the plugin will
-        // keep running, which shows the cancel button at the bottom of the screen.
-        // figma.closePlugin();
-    };
+        if (msg.type === 'send_commands') {
+            console.log("Get message from front end!!");
+            console.log(msg);
+            const text = figma.createText();
+            // Make sure the new text node is visible where we're currently looking
+            text.x = figma.viewport.center.x;
+            text.y = figma.viewport.center.y;
+            yield figma.loadFontAsync(text.fontName);
+            text.characters = yield msg.result_str;
+            // text.characters = await msg.result_str
+            // text.characters = "OYYYZZZZZHHHHHH"
+        }
+        // figma.closePlugin()
+    });
+    // Calls to "parent.postMessage" from within the HTML page will trigger this
+    // callback. The callback will be passed the "pluginMessage" property of the
+    // posted message.
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    // figma.closePlugin();
     // If the plugins isn't run in Figma, run this code
 }
 else {
